36f4fe91586db5153ca1061cc702808e147667ea
Luis Rioja Cáceres <7901045@alumnos.ufv.es>
2020-03-20 16:10:38 +0100

    Ahorcado completo pero arreglando fallos. Versión 0.8

--- Ahorcado/Form1.Designer.cs
+++ Ahorcado/Form1.Designer.cs
@@ -62,6 +62,7 @@
             // 
             // pictureBox1
             // 
+            this.pictureBox1.InitialImage = global::Ahorcado.Properties.Resources.ahorcado_0;
             this.pictureBox1.Location = new System.Drawing.Point(12, 94);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(736, 199);
@@ -70,10 +71,12 @@
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(193, 23);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(352, 68);
+            this.label1.Size = new System.Drawing.Size(736, 68);
             this.label1.TabIndex = 1;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button1
             // 
--- Ahorcado/Form1.cs
+++ Ahorcado/Form1.cs
@@ -12,9 +12,84 @@ namespace Ahorcado
 {
     public partial class Form1 : Form
     {
+
+        int numFallos = 0;
+        String palabraOculta = eligePalabra();
+        Boolean partidaTerminada = false;
+
+
         public Form1()
         {
             InitializeComponent();
+
+            String _palabraGuiones = "";
+            for (int i = 0; i < palabraOculta.Length; i++)
+            {
+                if (palabraOculta[i] != ' ')
+                {
+                    _palabraGuiones += "_ ";
+                }
+                else
+                {
+                    _palabraGuiones += "  ";
+                }
+                label1.Text = _palabraGuiones;
+            }
+
+
+
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private static String eligePalabra()
+        {
+            String[] listaPalabras = { "HolA", "VLADikaKA", "BaBy YOdA" };
+            Random aleatorio = new Random();
+            int pos = aleatorio.Next(listaPalabras.Length);
+            return listaPalabras[pos].ToUpper();
+        }
+
+        private void letraPulsada(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            String letra = boton.Text;
+            letra = letra.ToUpper();
+            if (palabraOculta.Contains(letra))
+            {
+                for (int i = 0; i < palabraOculta.Length; i++)
+                {
+                    if (palabraOculta[i] == letra[0])
+                    {
+                        label1.Text = label1.Text.Remove(2 * i, 1).Insert(2 * i, letra);
+                    }
+                }
+            }
+            else
+            {
+                numFallos++;
+            }
+
+            if (!label1.Text.Contains('_'))
+            {
+                numFallos = -100;
+            }
+            switch (numFallos)
+            {
+                case 0: pictureBox1.Image = Properties.Resources.ahorcado_0; break;
+                case 1: pictureBox1.Image = Properties.Resources.ahorcado_1; break;
+                case 2: pictureBox1.Image = Properties.Resources.ahorcado_2; break;
+                case 3: pictureBox1.Image = Properties.Resources.ahorcado_3; break;
+                case 4: pictureBox1.Image = Properties.Resources.ahorcado_4; break;
+                case 5: pictureBox1.Image = Properties.Resources.ahorcado_5; break;
+                case 6: pictureBox1.Image = Properties.Resources.ahorcado_fin; break;
+                case -100: pictureBox1.Image = Properties.Resources.acertasteTodo; break;
+                default: pictureBox1.Image = Properties.Resources.ahorcado_fin; break;
+
+            }
+        }
+
     }
 }
